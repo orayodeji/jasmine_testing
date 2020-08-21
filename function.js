863ae2908374d768057e00120b2c8614b0819cd4
orayodeji <orayodeji@gmail.com>
2020-08-21 10:06:42 +0100

    first commit

---
+++ function.js
@@ -0,0 +1,46 @@
+function replaceWith(string,charToReplace,charToReplaceWith){
+    //convert the strings to an array
+    var hop = string.split("");
+    //create a for loop to locate x and replace with y
+    for(var i = 0;i < hop.length; i++){
+    if(hop[i] === charToReplace){
+    hop[i] = charToReplaceWith}
+    }
+    return hop.join("")};
+
+
+        function expand(arr,num){
+            let newArray = [];
+            for(let i = 0; i < num ; i ++){
+                newArray.push(arr)
+            }
+            return newArray.flat();
+        }
+
+    
+        function acceptNumbersOnly(){
+            for(let i = 0; i < arguments.length; i++){
+            if (typeof arguments[i] !== "number"){ return false}
+            }
+            return true
+            };
+
+
+            function mergeArr(arr1,arr2){
+
+                let newArr = [];
+                newArr = arr1.concat(arr2);
+                newArr.sort(function(a,b){
+                  return  a - b
+                });
+                return newArr
+                
+            }
+
+
+            function mergeObj(obj1, obj2){
+                for(let key in obj2){
+                    obj1[key] = obj2[key]
+                }
+                return obj1
+            }
---
+++ functionSpec.js
@@ -0,0 +1,45 @@
+describe("replaceWith",function(){
+    it("returns a string",function(){
+        expect(replaceWith("awesome","w","y")).toEqual("ayesome")
+    })
+
+    it("replace a string",function(){
+        expect(replaceWith("Foo","F","B")).toEqual("Boo")
+    })
+})
+
+describe("expand",function(){
+    it("extends an array",function(){
+        expect(expand([1,2,3],3)).toEqual([1,2,3,1,2,3,1,2,3])
+    })
+    it("extends an array",function(){
+        expect(expand(["foo","test"],1)).toEqual(["foo","test"])
+    })
+})
+
+describe("acceptNumbersOnly",function(){
+    it("only returns boolean", function(){
+        expect(acceptNumbersOnly(1,"foo")).toBe(false)
+    })
+    it("only returns boolean", function(){
+        expect(acceptNumbersOnly(1,2,3,4,5,6,7)).toBe(true)
+    })
+    it("only returns boolean", function(){
+        expect(acceptNumbersOnly(1,2,3,4,5,6,NaN)).toBe(true)
+    })
+
+})
+
+describe("mergeArr", function(){
+    it("joins arrays and sort them", function(){
+        expect(mergeArr([2,1],[3,4])).toEqual([1,2,3,4])
+    })
+})
+
+describe("mergeObj", function(){
+    it("combine objects", function(){
+        expect(mergeObj(obj1= {name:"Foo",num: 33},
+                        obj2 = {test: "thing",num: 55})).toEqual({
+                        name: "Foo",test: "thing",num: 55})
+    })
+})
